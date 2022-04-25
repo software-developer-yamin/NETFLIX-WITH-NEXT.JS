70922f32c61d0d5e349a3a2183372df074f254fd
DeveloperYamin <developer.yamin@gmail.com>
2022-04-25 13:05:36 +0600

    server side rendering function !Done

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,8 +1,29 @@
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { baseUrl } from '../constants/movie'
+import { Movie } from '../typings'
 
-const Banner = () => {
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    )
+  }, [netflixOriginals])
+
   return (
     <section>
+      <div>
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          alt={movie?.title}
+        />
+      </div>
     </section>
   )
 }
